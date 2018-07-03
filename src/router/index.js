d68cc91931bedadd48f095dec55177a09f56e69a
hsimao <e087754958@gmail.com>
2018-07-03 20:33:55 +0800

    add dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,43 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Dashboard from '@/components/Dashboard';
 import Login from '@/components/pages/Login';
+import Products from '@/components/pages/Products';
 
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        // 如果輸入的網址是沒有定義的路由，則導到login頁面
-        {
-          path: '*',
-          redirect: 'login',
-        },
+  routes: [
+    // 如果輸入的網址是沒有定義的路由，則導到login頁面
+    {
+      path: '*',
+      redirect: 'login',
+    },
 
-        {
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld,
-            meta: { requiresAuth: true }
-        },
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+      meta: { requiresAuth: true }
+    },
 
-        {
-            path: '/login',
-            name: 'Login',
-            component: Login,
-        },
-    ],
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
+    },
+
+    {
+      path: '/admin',
+      name: 'Dashboard',
+      component: Dashboard,
+      children:[{
+        path: 'products',
+        name: 'Products',
+        component: Products,
+        meta: { requiresAuth: true },
+      }]
+    },
+  ],
 });
